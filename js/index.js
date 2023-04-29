89a8770b876dae177ada8bb0991c8ffe26a6d549
dr-moscow <dr.moscow@protonmail.com>
2023-04-29 22:59:00 +0300

    Сверстали popup но не привели в порядок
    Сверстан попап, но не работает еще.

--- js/index.js
+++ js/index.js
@@ -1,20 +1,20 @@
-const popuButton = document.querySelector ('poopup--menu-button');
-const popupWrapper = document.querySelector ('poopup--menu')
-const popupMenu = document.querySelector ('poopup--menu-list');
+const popupButton = document.querySelector ('.poopup--menu-button');
+const popupWrapper = document.querySelector ('.poopup--menu');
+const popupMenu = document.querySelector ('.poopup--menu-list');
 
-popuButton.addEventListener ('click', handleMenu);
+popupButton.addEventListener ('click', handleMenu);
 
 function handleMenu() {
 	popupMenu.classList.toggle('hide-poopup');
 }
 
-document.addEventListener ('click', hidePopup);
-function hidePopup (el) {
-	let targetInside = popupWrapper.contains(el.target);
-	if(!targetInside) {
-		popupMenu.classList.add('hide-poopup');
-	}
-	else {
-		return;
-	}
-}
+// document.addEventListener ('click', hidePopup);
+// function hidePopup(el) {
+// 	let targetInside = popupWrapper.contains(el.target);
+// 	if(!targetInside) {
+// 		popupMenu.classList.add('hide-poopup');
+// 	}
+// 	else {
+// 		return;
+// 	}
+// }
